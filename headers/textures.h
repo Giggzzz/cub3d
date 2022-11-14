0f4851ed010a087da7fa200a333dba28dce40962
Giggz <Giggz@prt>
2022-11-14 13:57:10 +0100

    tx

--- headers/textures.h
+++ headers/textures.h
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 11:58:14 by gudias            #+#    #+#             */
-/*   Updated: 2022/11/08 20:12:26 by gudias           ###   ########.fr       */
+/*   Updated: 2022/11/14 13:57:52 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define TEXTURES_H
 
 // minimap textures
-# define MM_PLAYER	"assets/textures/minimap/player.xpm"
+# define MM_PLAYER	"assets/textures/mm/player.xpm"
 # define MM_EMPTY	0x70303030
 # define MM_GROUND	0x00C1C1C1
 # define MM_WALL	0x00000000
@@ -24,10 +24,10 @@
 # define MM_ENEMY	0x00660000
 
 // default game textures
-# define TX_NORTH	"assets/textures/dark_brick.xpm"
-# define TX_SOUTH	"assets/textures/jungle.xpm"
-# define TX_EAST	"assets/textures/jungle_brick.xpm"
-# define TX_WEST	"assets/textures/handpainted_brick.xpm"
+# define TX_NORTH	"assets/textures/wolftx/bluestone.xpm"
+# define TX_SOUTH	"assets/textures/wolftx/colorstone.xpm"
+# define TX_EAST	"assets/textures/wolftx/purplestone.xpm"
+# define TX_WEST	"assets/textures/wolftx/greystone.xpm"
 # define TX_FLOOR	"assets/textures/dirt1.xpm"
 # define TX_CEIL	"assets/textures/sky_doubled.xpm"
 
@@ -57,11 +57,11 @@
 // POV (hands, weapons, ...)
 # define TX_PISTOL_NB	6
 
-# define TX_PISTOL1	"assets/textures/pistol1/01F.xpm"
-# define TX_PISTOL2	"assets/textures/pistol1/02F.xpm"
-# define TX_PISTOL3	"assets/textures/pistol1/03F.xpm"
-# define TX_PISTOL4	"assets/textures/pistol1/04F.xpm"
-# define TX_PISTOL5	"assets/textures/pistol1/05F.xpm"
-# define TX_PISTOL6	"assets/textures/pistol1/06F.xpm"
+# define TX_PISTOL1	"assets/textures/pov/pistol1/01.xpm"
+# define TX_PISTOL2	"assets/textures/pov/pistol1/02.xpm"
+# define TX_PISTOL3	"assets/textures/pov/pistol1/03.xpm"
+# define TX_PISTOL4	"assets/textures/pov/pistol1/04.xpm"
+# define TX_PISTOL5	"assets/textures/pov/pistol1/05.xpm"
+# define TX_PISTOL6	"assets/textures/pov/pistol1/06.xpm"
 
 #endif /* TEXTURES_H */
